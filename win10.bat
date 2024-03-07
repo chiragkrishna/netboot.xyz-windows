56b010dcf580da5bf2cb8a55f7ab832f59ae0d83
chiragkrishna <chiragkrishna@gmail.com>
2024-03-07 07:56:17 +0530

    Create win10.bat

---
+++ win10.bat
@@ -0,0 +1,9 @@
+@echo off
+echo Removing restrictions
+for %%s in (sCPU sSecureBoot sTPM) do reg add HKLM\SYSTEM\Setup\LabConfig /f /v Bypas%%sCheck /d 1 /t reg_dword
+echo Initializing network
+wpeinit
+echo Connecting to Shared Drive
+net use Z: \\10.11.12.10\server /user:name password
+echo Running Setup.exe
+Z:\docker_appdata\netbootxyz\assets\Win10\setup.exe
